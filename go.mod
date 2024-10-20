68b079dd2fbc9eba35193ffec428f5617c2a2086
iamCelott <dcviriya313@gmail.com>
2024-10-21 01:46:53 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module restful-golang
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleIndex(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	var port = "8000"
+	http.HandleFunc("/", handleIndex)
+
+
+	fmt.Printf("Server Started at Port %s\n", port)
+	http.ListenAndServe(":" + port, nil)
+}
